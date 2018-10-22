7e4c8b96f446278482d64737ae488d169c00f5ec
hakanAkyürek <hakanaska@gmail.com>
2018-10-22 15:13:44 +0300

    k Nearest neighbours function added.

--- Assignment1/kNN.py
+++ Assignment1/kNN.py
@@ -1,12 +1,31 @@
 from scipy import spatial
 
 
-def GetNeighbours():
-    print("empty")
-    #TODO: empty
+def GetNeighbours(dataset, testData, k):
 
+    count = 0
+
+    neightbours = []
+
+    for data in dataset:
+        sim = CosineBasedSimilarity(data, testData)
+        for i in range (0, k):
+
+            if(len(neightbours) == 0):
+                neightbours.append([data, sim])
+                break
+
+            elif len(neightbours) < k:
+                neightbours.append([data, sim])
+                break
+
+            elif(sim > neightbours[i][1]):
+                neightbours.pop(i)
+                neightbours.append([data, sim])
+                break
+
+    return neightbours
 
 def CosineBasedSimilarity(var1, var2):
     return 1 - spatial.distance.cosine(var1, var2)
-
 
